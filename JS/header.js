a091e84c9e912b89462bf1a32002a1773078612f
sacsry <leehv1234@kookmin.ac.kr>
2024-01-24 09:27:18 +0900

    ㅋㅋ

---
+++ JS/header.js
@@ -0,0 +1,4 @@
+let header = document.querySelector("#header");
+let rannum = Math.floor(Math.random()*4);
+let imglist1 = ["배경 이미지0.jpg","배경 이미지1.jpg","배경 이미지2.jpg","배경 이미지3.jpg","배경 이미지4.jpg"];
+header.style.backgroundImage = `url('./IMG/배경\ 이미지${rannum}.jpg')`;
